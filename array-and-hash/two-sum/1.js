3c872548a9e515d2b7fef86382bb2b26f6166c57
fmot <genzzabro09812@gmail.com>
2025-03-13 14:23:21 -0700

    😇

--- array-and-hash/two-sum/1.js
+++ array-and-hash/two-sum/1.js
@@ -1,21 +1,18 @@
 // O(n^2)になってるからパフォーマンス悪い
 
 class Solution {
-    /**
-     * @param {number[]} nums
-     * @param {number} target
-     * @return {number[]}
-     */
-    twoSum(nums, target) {
-    const prevMap = {}
-      for(let i = 0; i < nums.length; i++){
-        let diff = target - nums[i]
-        if(diff in prevMap){
-          return[prevMap[diff], i]
+  /**
+   * @param {number[]} nums
+   * @param {number} target
+   * @return {number[]}
+   */
+  twoSum(nums, target) {
+    for (let i = 0; i < nums.length - 1; i++) {
+      for (let j = 0; j < nums.length; j++) {
+        if (i !== j) {
+          if (nums[i] + nums[j] === target) return [i, j];
         }
-
-         prevMap[nums[i]] = i
       }
-      return []
     }
+  }
 }
