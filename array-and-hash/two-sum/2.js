bc4e10a4fb4b0c2afdbf392c39444479251b6bf1
fmot <genzzabro09812@gmail.com>
2025-03-13 14:24:26 -0700

    😇

--- array-and-hash/two-sum/2.js
+++ array-and-hash/two-sum/2.js
@@ -1,5 +1,4 @@
 // Hash Mapで解いたパターン
-
 class Solution {
   /**
    * @param {number[]} nums
@@ -7,12 +6,15 @@ class Solution {
    * @return {number[]}
    */
   twoSum(nums, target) {
-    for (let i = 0; i < nums.length - 1; i++) {
-      for (let j = 0; j < nums.length; j++) {
-        if (i !== j) {
-          if (nums[i] + nums[j] === target) return [i, j];
-        }
+    const prevMap = {};
+    for (let i = 0; i < nums.length; i++) {
+      let diff = target - nums[i];
+      if (diff in prevMap) {
+        return [prevMap[diff], i];
       }
+
+      prevMap[nums[i]] = i;
     }
+    return [];
   }
 }
